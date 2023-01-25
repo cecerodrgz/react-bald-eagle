7cb328d6bd041294e7d24d0546c9403e6a8974f4
Cecilia Rodriguez <cecerodrgz@Cecilias-Air.lan>
2023-01-24 19:51:51 -0500

    lines

--- src/AddTodoForm.js
+++ src/AddTodoForm.js
@@ -1,41 +1,33 @@
 import React from "react";
-import {useState} from 'react'; 
+import { useState } from "react";
 import { InputWithLabel } from "./InputWithLabel";
 
-export default function AddTodoForm({ onAddTodo }){
-    const [todoTitle, setTodoTitle] = useState('');
-    const handleTitleChange = (e) => {
-        const newTodoTitle = e.target.value;
-        setTodoTitle(newTodoTitle);
-    }
-     const handleAddTodo = (event) =>{
-         event.preventDefault();
-            const todoObject = {
-                title: todoTitle, 
-                id: Date.now()
-            }
+export default function AddTodoForm({ onAddTodo }) {
+  const [todoTitle, setTodoTitle] = useState("");
+  const handleTitleChange = (e) => {
+    const newTodoTitle = e.target.value;
+    setTodoTitle(newTodoTitle);
+  };
+  const handleAddTodo = (event) => {
+    event.preventDefault();
+    const todoObject = {
+      title: todoTitle,
+      id: Date.now(),
+    };
 
-            onAddTodo(todoObject);
-            setTodoTitle('');
-     };
+    onAddTodo(todoObject);
+    setTodoTitle("");
+  };
 
-     return (
-         <form onSubmit={handleAddTodo}>
-             {/* <label htmlFor="todoTitle">Title</label>
-             <input 
-             type='text' 
-             id="TodoTitle" 
-             name="title"
-             value={todoTitle}
-             onChange={handleTitleChange}
-             /> */}
-             <InputWithLabel
-                todoTitle={todoTitle}
-                handleTitleChange={handleTitleChange}
-                >
-                    <b>Title:</b>
-                </InputWithLabel>
-             <button type="submit">Add</button>
-         </form>
-     )
- }
+  return (
+    <form onSubmit={handleAddTodo}>
+      <InputWithLabel
+        todoTitle={todoTitle}
+        handleTitleChange={handleTitleChange}
+      >
+        <b>Title:</b>
+      </InputWithLabel>
+      <button type="submit">Add</button>
+    </form>
+  );
+}
