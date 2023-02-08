36adbcfbb366745f7576e3e18c49055e72d125f0
Cecilia Rodriguez <cecerodrgz@Cecilias-Air.lan>
2023-02-07 20:17:56 -0500

    lesson-3-1

--- src/AddTodoForm.js
+++ src/AddTodoForm.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { useState } from "react";
 import { InputWithLabel } from "./InputWithLabel";
+import style from './AddTodoForm.module.css'
 
 export default function AddTodoForm({ onAddTodo }) {
-  const [todoTitle, setTodoTitle] = useState("");
+  const [todoTitle, setTodoTitle] = useState('');
   const handleTitleChange = (e) => {
     const newTodoTitle = e.target.value;
     setTodoTitle(newTodoTitle);
@@ -16,18 +17,18 @@ export default function AddTodoForm({ onAddTodo }) {
     };
 
     onAddTodo(todoObject);
-    setTodoTitle("");
+    setTodoTitle('');
   };
 
   return (
     <form onSubmit={handleAddTodo}>
-      <InputWithLabel
-        todoTitle={todoTitle}
-        handleTitleChange={handleTitleChange}
-      >
-        <b>Title:</b>
-      </InputWithLabel>
-      <button type="submit">Add</button>
+        <InputWithLabel
+           todoTitle={todoTitle}
+           handleTitleChange={handleTitleChange}
+           >
+               <b className={style.Title}>Title:</b>
+           </InputWithLabel>
+        <button type="submit" className={style.btn}>Add</button>
     </form>
-  );
+)
 }
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,9 +1,10 @@
 import React from "react";
 import TodoListItem from "./TodoListItem";
+import style from "./TodoList.module.css"
 
 export default function Todolist({ todoList, onRemoveTodo }) {
   return (
-    <ul>
+    <ul className={style.ListItem}>
       {todoList.map((todoListItem) => (
         <TodoListItem
           key={todoListItem.id}
